cb0847339e29166c854508821113795400dc5bb4
Gustavo Feriani <guuh.feriani@gmail.com>
2023-10-23 20:56:32 -0300

    upload turma s-

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/* / tres constantes para referenciar as "ITEM" divs Projetos
+const form = document.getElementById('form');  
+// "const" = Escopo Global - boa pratica é "declar" variavel
+const site = document.getElementById('site');
+const port = document.getElementById('port');
+const movieform = document.querySelector('.movieform');
+const moviesite = document.querySelector('.moviesite');
+const movieport = document.querySelector('.movieport');
+
+
+// evento de click
+form.addEventListener("click",() => {
+    form.classList.toggle('active');
+    movieform.classList.toggle('active');
+});
+site.addEventListener("click",() => {
+    site.classList.toggle('active');
+    moviesite.classList.toggle('active');
+});
+port.addEventListener("click",() => {
+    port.classList.toggle('active');
+    movieport.classList.toggle('active');
+});
+
+*/
